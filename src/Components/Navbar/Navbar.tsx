2d77ee047c9efe926ee0a7282a03ab8e2886318b
piyush2053 <piyushpatel2053@gmail.com>
2025-06-15 00:13:09 +0530

    Logo updated

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 import { Fragment } from "react/jsx-runtime";
-import LOGO from '../../Assets/Logo/2.png'
+import LOGO from '../../Assets/Logo/3.png'
 
 export default function Navbar() {
     return (
         <Fragment>
-            <div className={`flex items-center justify-between py-[4px] px-5 text-white bg-white`}>
+            <div className={`flex items-center justify-between py-4 px-5 text-white !bg-black`}>
                 <div className="flex items-center ">
                     <img  src={LOGO} alt="kencarltonlogo" className="h-10"/>
                 </div>
